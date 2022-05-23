de73f0860da4733de783883b5a154e1add985b87
codefactor-io <support@codefactor.io>
2022-05-23 01:26:46 +0000

    [CodeFactor] Apply fixes

--- src/find-one.ts
+++ src/find-one.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function findOne() {
-  const findOne = await prisma.courses.findFirst()
-  console.log(findOne)
+  const findOne = await prisma.courses.findFirst();
+  console.log(findOne);
 }
-findOne()
+findOne();
